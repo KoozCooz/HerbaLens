6ad437298f884080df81e7f460feb5b00597597b
KoozCooz <cristalkathy@gmail.com>
2023-11-28 18:10:17 +0800

    Edited profile part

--- herbalens/lib/ui/screens/Profile/profile_part.dart
+++ herbalens/lib/ui/screens/Profile/profile_part.dart
@@ -48,139 +48,110 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return WillPopScope(
       onWillPop: () async{
         return false;
       },
       child:Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          height: size.height,
-          width: size.width,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 150,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: const Color.fromRGBO(41, 110, 72, 1).withOpacity(.5),
-                          width: 2,
-                        ),
-                      ),
-                      child: const CircleAvatar(
-                        radius: 60,
-                        backgroundColor: Colors.transparent,
-                        backgroundImage: ExactAssetImage(
-                            'assets/images/default_profile.jpg'),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 150,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: const Color.fromRGBO(41, 110, 72, 1).withOpacity(.5),
+                        width: 2,
                       ),
                     ),
-                    Positioned(
-                      bottom: 0,
-                      right: 10,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).scaffoldBackgroundColor,
-                          ),
-                          color: const Color.fromRGBO(41, 110, 72, 1),
-                        ),
-                        child: const Icon(
-                          Icons.add_a_photo,
-                          color: Colors.white,
-                        ),
-                      ),
+                    child: const CircleAvatar(
+                      radius: 60,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: ExactAssetImage(
+                          'assets/images/default_profile.jpg'),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 8), // Add space after the Stack
+            ),
+            const SizedBox(height: 8), // Add space after the Stack
 
-              // add plus button to change profile picture
-
-              Text(
-                //John Doe,
-                _nameController.text,
-                style: TextStyle(
-                  color: Constants.blackColor,
-                  fontSize: 20,
-                ),
+            Text(
+              //John Doe,
+              _nameController.text,
+              style: TextStyle(
+                color: Constants.blackColor,
+                fontSize: 20,
               ),
-              //EMAIL
-              Text(
-                //'johndoe@gmail.com',
-                _emailController.text,
-                style: TextStyle(
-                  color: Constants.blackColor.withOpacity(.3),
-                ),
+            ),
+            //EMAIL
+            Text(
+              //'johndoe@gmail.com',
+              _emailController.text,
+              style: TextStyle(
+                color: Constants.blackColor.withOpacity(.3),
               ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              SizedBox(
-                height: size.height * .7,
-                width: size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //EDIT SETTINGS
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, PageTransition(child: const Settings(), type: PageTransitionType.bottomToTop));
-                      },
-                      child: const ProfileWidget(
-                        icon: Icons.settings,
-                        title: 'Settings',
-                      ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            SizedBox(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //EDIT SETTINGS
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, PageTransition(child: const Settings(), type: PageTransitionType.bottomToTop));
+                    },
+                    child: const ProfileWidget(
+                      icon: Icons.settings,
+                      title: 'Settings',
                     ),
+                  ),
 
-                    //EDIT FAQS
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, PageTransition(child: FAQPage(), type: PageTransitionType.bottomToTop));
-                      },
-                      child: const ProfileWidget(
-                        icon: Icons.chat,
-                        title: 'FAQs',
-                      ),
+                  //EDIT FAQS
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, PageTransition(child: FAQPage(), type: PageTransitionType.bottomToTop));
+                    },
+                    child: const ProfileWidget(
+                      icon: Icons.chat,
+                      title: 'FAQs',
                     ),
+                  ),
 
-                    //SHARE HERBALENS APP APK/GOOGLE PLAY TO FRIENDS QR OR LINK
-                    GestureDetector(
-                      onTap: () {
-                        // Add code to perform desired action when "Share" is tapped
-                      },
-                      child: const ProfileWidget(
-                        icon: Icons.share,
-                        title: 'Share',
-                      ),
+                  //SHARE HERBALENS APP APK/GOOGLE PLAY TO FRIENDS QR OR LINK
+                  GestureDetector(
+                    onTap: () {
+                      // Add code to perform desired action when "Share" is tapped
+                    },
+                    child: const ProfileWidget(
+                      icon: Icons.share,
+                      title: 'Share',
                     ),
+                  ),
 
-                    //LOG OUT
-                    GestureDetector(
-                      onTap: () async {
-                        await selectLogOut(context);
-                      },
-                      child: const ProfileWidget(
-                        icon: Icons.logout,
-                        title: 'Log Out',
-                      ),
-                    )
-                  ],
-                ),
+                  //LOG OUT
+                  GestureDetector(
+                    onTap: () async {
+                      await selectLogOut(context);
+                    },
+                    child: const ProfileWidget(
+                      icon: Icons.logout,
+                      title: 'Log Out',
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     ),
