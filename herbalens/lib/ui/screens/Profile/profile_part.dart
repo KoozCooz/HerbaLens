fec4c5dbcf0abd30b5dfddc04e836365dcce8df3
KoozCooz <cristalkathy@gmail.com>
2023-12-03 14:22:44 +0800

    Edit

--- herbalens/lib/ui/screens/Profile/profile_part.dart
+++ herbalens/lib/ui/screens/Profile/profile_part.dart
@@ -5,6 +5,7 @@ import 'package:herbalens/Firebase_auth_implementation/firebase_auth_services.da
 import 'package:herbalens/constants.dart';
 import 'package:herbalens/ui/screens/Profile/Settings/settings.dart';
 import 'package:herbalens/ui/screens/Profile/FAQs/faqs-ui.dart';
+import 'package:herbalens/ui/screens/Profile/share.dart';
 import 'package:herbalens/ui/screens/widgets/profil_widget.dart';
 import 'package:herbalens/ui/screens/Account/signin_page.dart';
 import 'package:page_transition/page_transition.dart';
@@ -20,7 +21,6 @@ class _ProfilePageState extends State<ProfilePage> {
   final FirebaseAuthService _auth = FirebaseAuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
-
 
   @override
   void initState() {
@@ -49,113 +49,125 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async{
+      onWillPop: () async {
         return false;
       },
-      child:Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: Stack(
-                children: [
-                  Container(
-                    width: 150,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: const Color.fromRGBO(41, 110, 72, 1).withOpacity(.5),
-                        width: 2,
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 150,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: const Color.fromRGBO(41, 110, 72, 1)
+                              .withOpacity(.5),
+                          width: 2,
+                        ),
+                      ),
+                      child: const CircleAvatar(
+                        radius: 60,
+                        backgroundColor: Colors.transparent,
+                        backgroundImage: ExactAssetImage(
+                            'assets/images/default_profile.jpg'),
                       ),
                     ),
-                    child: const CircleAvatar(
-                      radius: 60,
-                      backgroundColor: Colors.transparent,
-                      backgroundImage: ExactAssetImage(
-                          'assets/images/default_profile.jpg'),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 8), // Add space after the Stack
+              const SizedBox(height: 8), // Add space after the Stack
 
-            Text(
-              //John Doe,
-              _nameController.text,
-              style: TextStyle(
-                color: Constants.blackColor,
-                fontSize: 20,
+              Text(
+                //John Doe,
+                _nameController.text,
+                style: TextStyle(
+                  color: Constants.blackColor,
+                  fontSize: 20,
+                ),
               ),
-            ),
-            //EMAIL
-            Text(
-              //'johndoe@gmail.com',
-              _emailController.text,
-              style: TextStyle(
-                color: Constants.blackColor.withOpacity(.3),
+              //EMAIL
+              Text(
+                //'johndoe@gmail.com',
+                _emailController.text,
+                style: TextStyle(
+                  color: Constants.blackColor.withOpacity(.3),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            SizedBox(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  //EDIT SETTINGS
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, PageTransition(child: const Settings(), type: PageTransitionType.bottomToTop));
-                    },
-                    child: const ProfileWidget(
-                      icon: Icons.settings,
-                      title: 'Settings',
-                    ),
-                  ),
-                  
-
-                  //EDIT FAQS
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, PageTransition(child: FaqsListScreen(), type: PageTransitionType.bottomToTop));
-                    },
-                    child: const ProfileWidget(
-                      icon: Icons.chat,
-                      title: 'FAQs',
-                    ),
-                  ),
-
-                  //SHARE HERBALENS APP APK/GOOGLE PLAY TO FRIENDS QR OR LINK
-                  GestureDetector(
-                    onTap: () {
-                      // Add code to perform desired action when "Share" is tapped
-                    },
-                    child: const ProfileWidget(
-                      icon: Icons.share,
-                      title: 'Share',
-                    ),
-                  ),
-
-                  //LOG OUT
-                  GestureDetector(
-                    onTap: () async {
-                      await selectLogOut(context);
-                    },
-                    child: const ProfileWidget(
-                      icon: Icons.logout,
-                      title: 'Log Out',
-                    ),
-                  )
-                ],
+              const SizedBox(
+                height: 15.0,
               ),
-            ),
-          ],
+              SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    //EDIT SETTINGS
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                child: const Settings(),
+                                type: PageTransitionType.bottomToTop));
+                      },
+                      child: const ProfileWidget(
+                        icon: Icons.settings,
+                        title: 'Settings',
+                      ),
+                    ),
+
+                    //EDIT FAQS
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                child: FaqsListScreen(),
+                                type: PageTransitionType.bottomToTop));
+                      },
+                      child: const ProfileWidget(
+                        icon: Icons.chat,
+                        title: 'FAQs',
+                      ),
+                    ),
+
+                    //SHARE HERBALENS APP APK/GOOGLE PLAY TO FRIENDS QR OR LINK
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                child: Share(),
+                                type: PageTransitionType.bottomToTop));
+                      }, //Share
+                      child: const ProfileWidget(
+                        icon: Icons.share,
+                        title: 'Share',
+                      ),
+                    ),
+
+                    //LOG OUT
+                    GestureDetector(
+                      onTap: () async {
+                        await selectLogOut(context);
+                      },
+                      child: const ProfileWidget(
+                        icon: Icons.logout,
+                        title: 'Log Out',
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
@@ -179,15 +191,17 @@ Future selectLogOut(BuildContext context) {
           TextButton(
             child: const Text('Logout'),
             onPressed: () async {
-              if (user != null && user.providerData.any((info) => info.providerId == 'google.com')) {
+              if (user != null &&
+                  user.providerData
+                      .any((info) => info.providerId == 'google.com')) {
                 // Sign out from Google
                 final GoogleSignIn googleSignIn = GoogleSignIn();
                 await googleSignIn.signOut();
               }
-              
+
               // Sign out from Firebase
               await FirebaseAuth.instance.signOut();
-              
+
               Navigator.pushReplacement(
                 context,
                 PageTransition(
